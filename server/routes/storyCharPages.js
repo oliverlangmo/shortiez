0bdd0c41dc7a265d14d61f82393eeaee35c4379e
mculotta120 <margaret.culotta@gmail.com>
2016-07-25 11:02:46 -0500

    working on add pages feature.

--- server/routes/storyCharPages.js
+++ server/routes/storyCharPages.js
@@ -20,6 +20,31 @@ router.post( '/addStory', function( req, res ){  // POST call
 
   var newStory=addStory( storyToAdd );  // saves record to database
   newStory.save();
-});
+}); //end addStory post
+
+router.post( '/addCharacter', function( req, res ){  // POST call
+  var characterToAdd={  // adds record from input
+    character_name: req.body.character_name,
+    character_traits: req.body.character_traits,
+    character_photo: req.body.character_photo
+  };
+  var charactersArray = [];
+  console.log(characterToAdd, "characterToAdd");
+
+  charactersArray.push(characterToAdd);
+
+  addStory.findOneAndUpdate({_id:"579287d62c2e099585a2503f"}, { story_characters:   charactersArray  }, function(err, issueResult){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+    // console.log(req.body.id, " found.", req.body.pages, "available")
+    console.log("success, check DB", charactersArray, "charactersArray");
+    res.sendStatus(200);
+    }
+  }); //end findOneAndUpdate
+}); //end character post
+
+
 
 module.exports = router;
